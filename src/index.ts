fe76b2ecb4f786b6bb827aa1032c1e1e6f28be86
Adham Tarek <adhamtarek291@gmail.com>
2024-08-27 14:10:02 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { getSubtitles } from "@treeee/youtube-caption-extractor";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/subtitles", async (c) => {
+  const { videoId, lang = "en" } = c.req.query();
+
+  if (!videoId) {
+    return c.json("Video ID is required", 400);
+  }
+
+  try {
+    const subtitles = await getSubtitles({ videoID: videoId, lang });
+    return c.json(subtitles, 200);
+  } catch (error) {
+    return c.json({ error: (error as Error).message }, 500);
+  }
+});
+
+export default app;

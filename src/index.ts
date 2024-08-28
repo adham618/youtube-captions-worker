6f0f0786129be7f7eabeb3ffd2b4e78e4c0e3eb2
Adham Tarek <adhamtarek291@gmail.com>
2024-08-28 21:59:46 +0300

    change to youtubei.js

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,40 @@
-import { getSubtitles } from "@treeee/youtube-caption-extractor";
 import { Hono } from "hono";
-import { cors } from "hono/cors";
 
 const app = new Hono();
 
-app.use(cors());
+import { Innertube } from "youtubei.js/cf-worker";
+
+type Response = {
+  text: string | undefined;
+  start: number;
+  end: number;
+};
+
+const fetchTranscript = async (
+  videoId: string,
+  lang: string
+): Promise<(Response | undefined)[] | undefined> => {
+  const youtube = await Innertube.create({
+    lang: lang,
+    location: "US",
+    retrieve_player: false,
+  });
+
+  try {
+    const info = await youtube.getInfo(videoId);
+    const transcriptData = await info.getTranscript();
+    return transcriptData?.transcript?.content?.body?.initial_segments.map(
+      (segment) => ({
+        text: segment.snippet.text,
+        start: Number(segment.start_ms),
+        end: Number(segment.end_ms),
+      })
+    );
+  } catch (error) {
+    console.error("Error fetching transcript:", error);
+    throw error;
+  }
+};
 
 app.get("/subtitles", async (c) => {
   const { videoId, lang = "en" } = c.req.query();
@@ -14,7 +44,8 @@ app.get("/subtitles", async (c) => {
   }
 
   try {
-    const subtitles = await getSubtitles({ videoID: videoId, lang });
+    const subtitles = await fetchTranscript(videoId, lang);
+
     return c.json(subtitles, 200);
   } catch (error) {
     return c.json({ error: (error as Error).message }, 500);
